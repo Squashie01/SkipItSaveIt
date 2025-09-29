91216e84c87f1a2295b546e2fda6763042849d81
Squashie <gummybears661@gmail.com>
2025-09-29 10:37:24 -0400

    update 3

--- app/capacitor.config.ts
+++ app/capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'skipitsaveit.app',
+  appId: '67e0f072',
   appName: '\x16\x16Skip It Save It',
   webDir: 'build'
 };
